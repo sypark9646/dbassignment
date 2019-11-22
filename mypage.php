485e3335459a45cd84210f50ec14bc1b587546ee
soyeon Park <sypark9646@gmail.com>
2019-11-22 21:12:37 +0900

    no message
    Signed-off-by: soyeon Park <sypark9646@gmail.com>

--- mypage.php
+++ mypage.php
@@ -31,13 +31,19 @@ button {
             padding-top: 16px;
         }
 </style>
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
 </head>  
-<body>
+<body style="background-color:#f1f1f1;>>
 <form action="updateUserInfo.php" method="post">
-<center><h2> <span style="background-color:#f1f1f1; padding:20px;">
-Today's weight <input type="number" step=0.01 name="weight" required>(kg) </span>
+<center><h2>
+Today's weight <input type="number" step=0.01 name="weight" required>(kg)
 <button type="submit">UPDATE</button>
 </h2></center><br>
 </form>
+<center>
+<button style='font-size:24px; background-color: #333; padding=50px;'>SHOW My Favorite Recipe!!!!<i class='fas fa-pizza-slice'></i></button>
+<button style='font-size:24px; background-color: #333; padding=50px;'>SHOW My Favorite Exercise!<i class='fas fa-basketball-ball'></i></button>
+</center>
+
 </body>
 </html>
