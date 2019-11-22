74e3e2be8a14ca598d9b622f6bd3bcb3df4fa725
soyeon Park <sypark9646@gmail.com>
2019-11-22 19:42:33 +0900

    no message
    Signed-off-by: soyeon Park <sypark9646@gmail.com>

--- posting.php
+++ posting.php
@@ -10,20 +10,15 @@ $content=$_POST['content'];
 $id=$_SESSION["userid"];
 $date=date('Y/m/d-H:i'); 
 
-$mysqli=mysqli_connect("localhost","root","","dbassignment");
-if (mysqli_connect_error()) {
-             printf("Connect failed: %s\n",mysqli_connect_error());
-             exit();
+include 'db_connection.php';
+
+$sql = "INSERT INTO boarddb(b_id, b_title, b_content, b_date, b_views, u_id) VALUES(NULL, '$title', '$content', '$date', 0, '$id')";
+$res = mysqli_query($mysqli,$sql);
+if ($res) {
+	echo "<center><h2>Posting is completed!</h2></center><br>";
+            header('Location: ./board.php');
 }
 else {
-             $sql = "INSERT INTO boarddb(b_id, b_title, b_content, b_date, b_views, u_id) VALUES(NULL, '$title', '$content', '$date', 0, '$id')";
-             $res = mysqli_query($mysqli,$sql);
-             if ($res) {
-		echo "<center><h2>Posting is completed!</h2></center><br>";
-                         header('Location: ./board.php');
-             }
-	else {
-		echo "<center><h2>Posting is failed!</h2></center><br>";
-	}
+	echo "<center><h2>Posting is failed!</h2></center><br>";
 }
 ?>
