1975f964f313c85d17e811f16a4906b9ccfa2c2d
soyeon Park <sypark9646@gmail.com>
2019-11-25 22:19:40 +0900

    no message

--- mypage.php
+++ mypage.php
@@ -4,7 +4,22 @@ $id=$_SESSION['userid'];
 include 'db_connection.php';
 include('navigationbar.html');
 include('updateUserinfo.html');
-
+$result = mysqli_query($mysqli, "SELECT u_category, SUM(u_cal) as sum FROM usercaldb where u_id='$id' GROUP BY u_category;");
+echo"<center><h2>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+	if($row['u_category']==0){
+		echo "calorie sum of exercise ".$row['sum']."<br>";
+	}
+	else{
+		echo "calorie sum of food ".$row['sum'];
+	}
+    }
+} else {
+    echo "0 results";
+}
+echo"</h2></center>";
 include('mypage_favorites_buttons.html');
 include 'draw_weight.php';
 ?>
