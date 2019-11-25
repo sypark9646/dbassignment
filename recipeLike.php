3c1bc184629a249e258da04bd49d48160632c362
Song-Jihyun <50199997+Song-Jihyun@users.noreply.github.com>
2019-11-25 22:10:04 +0900

    Update recipeLike.php

--- recipeLike.php
+++ recipeLike.php
@@ -3,10 +3,10 @@
   $mysqli->set_charset('utf8');
   $d_id = $_GET['d_id'];
   $id=$_SESSION['userid']
-  /*transaction �ʿ�*/
+  /*transaction ÇÊ¿ä*/
   $sql = 'UPDATE dietdb set d_up = d_up + 1 where d_id = ' . $d_id;
   $res = $mysqli->query($sql);
-  $signup2=mysqli_query($mysqli,"INSERT INTO userfavoritesdb(u_id, u_favorite_id, u_category) VALUES ('$id', '$d_id', 0)");
+  $signup2=mysqli_query($mysqli,"INSERT INTO userfavoritesdb(u_id, u_favorite_id, u_category) VALUES ('$id', '$d_id', 0) ON DUPLICATE KEY UPDATE u_category = u_category");
   if ($res) {
 		echo "<center><h2>Like is completed!</h2></center><br>";
                          header('Location: ./recipeDetail.php?d_id=' . $d_id.'');
