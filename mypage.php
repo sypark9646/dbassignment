f1ee377eaf506bc37820155b8e3dd4d328348743
soyeon Park <sypark9646@gmail.com>
2019-11-25 22:27:39 +0900

    avg

--- mypage.php
+++ mypage.php
@@ -17,9 +17,26 @@ if ($result->num_rows > 0) {
 	}
     }
 } else {
-    echo "0 results";
+    echo "There's no sum of calorie";
 }
-echo"</h2></center>";
+
+$result = mysqli_query($mysqli, "SELECT u_category, Avg(u_cal) as avg 
+FROM usercaldb GROUP BY u_category");
+echo"<center>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+	if($row['u_category']==0){
+		echo "calorie avg of exercise ".$row['avg']."<br>";
+	}
+	else{
+		echo "calorie avg of food ".$row['avg'];
+	}
+    }
+} else {
+    echo "There's no avg of calorie";
+}
+echo"</center>";
 include('mypage_favorites_buttons.html');
 include 'draw_weight.php';
 ?>
