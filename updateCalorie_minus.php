cc7edc37210bd88fead75f7cf03b3c55c8a1d55c
Song-Jihyun <50199997+Song-Jihyun@users.noreply.github.com>
2019-11-25 21:49:00 +0900

    Update updateCalorie_minus.php

--- updateCalorie_minus.php
+++ updateCalorie_minus.php
@@ -9,21 +9,32 @@ $id=$_SESSION['userid'];
   $date=date('Y/m/d');
 
 /*이부분 transaction*/
-  $sqlzero="INSERT INTO usercaldb(u_num, u_id, u_category, u_cal) VALUES (NULL, '$u_id', 0, '$e_cal')";
-  $result = $mysqli->query($sqlzero);
-
+try{
+  $mysqli->autocommit(FALSE);
+  $transactionres=$mysqli->query("INSERT INTO usercaldb(u_num, u_id, u_category, u_cal) VALUES (NULL, '$u_id', 0, '$e_cal')");
+  if(!$transactionres){
+    $transactionres->free();
+    throw new Exception($mysqli->error);
+  }
+  
   $sqlone="SELECT u_weight from userinfodb where u_id='".$id."' order by u_date desc limit 1";
   $result = $mysqli->query($sqlone);
   $row = $result->fetch_assoc();
   $weight=$row['u_weight'];
-  $sql="INSERT INTO userinfodb(u_id, u_date, u_weight, u_calSum) VALUES ('$id', '$date', '$weight', '-$e_cal') ON DUPLICATE KEY UPDATE u_calSum = u_calSum - '$e_cal'";
-  $signup2=mysqli_query($mysqli,$sql);
-  
-if ($signup2) {
-		echo "<center><h2>Like is completed!</h2></center><br>";
+  $transactionres=$mysqli->query("INSERT INTO userinfodb(u_id, u_date, u_weight, u_calSum) VALUES ('$id', '$date', '$weight', '-$e_cal') ON DUPLICATE KEY UPDATE u_calSum = u_calSum - '$e_cal'");
+  if(!$transactionres){
+    $transactionres->free();
+    throw new Exception($mysqli->error);
+  }
+
+  $mysqli->commit();
+  $mysqli->autocommit(TRUE);
+
+  echo "<center><h2>Like is completed!</h2></center><br>";
                          header('Location: ./exerciseDetail.php?e_id=' . $e_id.'');
-             }
-	else {
-		echo "<center><h2>Update Calorie has failed!</h2></center><br>";
-	}
+}catch(Exception $e){
+  $mysqli->rollback();
+  $mysqli->autocommit(TRUE);
+  echo "<center><h2>Update Calorie has failed!</h2></center><br>";
+}
 ?>
